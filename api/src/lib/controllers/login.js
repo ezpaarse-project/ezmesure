b10adf969bdd9afd2fcdc38d620da8aa920da4ac
Yannick Schurter <yannick.schurter@gmail.com>
2016-05-31 14:07:08 +0200

    add more details in the token

--- api/src/lib/controllers/login.js
+++ api/src/lib/controllers/login.js
@@ -5,16 +5,19 @@ export default function* renaterLogin() {
     const headers = this.request.header;
 
     const user = {
-      username: headers.remote_user
+      uid: headers.uid,
+      username: headers.displayname,
+      mail: headers.mail,
+      org: headers.o,
+      unit: headers.ou,
+      eppn: headers.eppn,
+      affiliation: headers.affiliation
     };
 
     if (!user) { return this.status = 401; }
 
     const cookie = headers.cookie && headers.cookie.split(';').filter(value => value.startsWith('_shibsession_'))[0];
-
-    const token = jwt.sign({
-      username: headers.remote_user
-    }, auth.secret);
+    const token  = jwt.sign(user, auth.secret);
 
     this.body = { token, user };
     // this.redirect(`${decodeURIComponent(this.query.origin)}?shib=${encodeURIComponent(cookie)}&token=${token}&username=${encodeURIComponent(headers.remote_user)}`);
