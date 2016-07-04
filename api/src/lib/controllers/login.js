aa5569d4920cd8d9a6b5687c481a6014f3492a70
Yannick Schurter <yannick.schurter@gmail.com>
2016-07-04 16:11:30 +0200

    token in cookies + redirect instead of json

--- api/src/lib/controllers/login.js
+++ api/src/lib/controllers/login.js
@@ -14,11 +14,6 @@ export default function* renaterLogin() {
       affiliation: headers.affiliation
     };
 
-    if (!user) { return this.status = 401; }
-
-    const cookie = headers.cookie && headers.cookie.split(';').filter(value => value.startsWith('_shibsession_'))[0];
-    const token  = jwt.sign(user, auth.secret);
-
-    this.body = { token, user };
-    // this.redirect(`${decodeURIComponent(this.query.origin)}?shib=${encodeURIComponent(cookie)}&token=${token}&username=${encodeURIComponent(headers.remote_user)}`);
+    this.cookies.set('eztoken', jwt.sign(user, auth.secret));
+    this.redirect(decodeURIComponent(this.query.origin || '/'));
 };
