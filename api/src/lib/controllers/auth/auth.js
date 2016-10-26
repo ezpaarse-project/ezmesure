981d25f327188bb6cb7fea74606e5b821eb828fb
Yannick Schurter <yannick.schurter@gmail.com>
2016-10-26 16:13:22 +0200

    user-level auth

--- api/src/lib/controllers/auth/auth.js
+++ api/src/lib/controllers/auth/auth.js
@@ -4,27 +4,33 @@ import mongo from '../../services/mongo';
 
 export function* renaterLogin() {
   const headers = this.request.header;
-  const idp     = headers['shib-identity-provider'];
+  const props   = {
+    idp:          headers['shib-identity-provider'],
+    uid:          headers.uid,
+    name:         headers.displayname || headers.cn || headers.givenname,
+    mail:         headers.mail,
+    org:          headers.o,
+    unit:         headers.ou,
+    eppn:         headers.eppn,
+    remoteUser:   headers.remote_user,
+    persistentId: headers['persistent-id'] || headers['targeted-id'],
+    affiliation:  headers.affiliation
+  };
 
-  if (!idp) {
+  if (!props.idp) {
     return this.throw('IDP not found in Shibboleth headers', 400);
   }
 
-  let user = yield mongo.db.collection('users').findOne({ idp });
+  let user = yield findUser(props);
 
   if (!user) {
-    yield mongo.db.collection('users').insert({
-      idp,
-      uid:            headers.uid,
-      name:           headers.displayname,
-      mail:           headers.mail,
-      org:            headers.o,
-      unit:           headers.ou,
-      eppn:           headers.eppn,
-      affiliation:    headers.affiliation
-    });
+    const result = yield mongo.db.collection('users').insert(props);
 
-    user = yield mongo.db.collection('users').findOne({ idp });
+    user = result && result.ops && result.ops[0];
+
+    if (!user) {
+      return this.throw('Failed to save user data', 500);
+    }
   }
 
   this.cookies.set('eztoken', jwt.sign(user, auth.secret), { httpOnly: true });
@@ -32,11 +38,10 @@ export function* renaterLogin() {
 };
 
 export function* getUser() {
-
-  const user = yield mongo.db.collection('users').findOne({ idp: this.state.user.idp });
+  const user = yield findUser(this.state.user);
 
   if (!user) {
-    return this.throw('IDP unregistered, please log in again', 401);
+    return this.throw('Unable to fetch user data, please log in again', 401);
   }
 
   this.status = 200;
@@ -47,3 +52,27 @@ export function* generateToken() {
   this.status = 200;
   this.body   = jwt.sign(this.state.user, auth.secret);
 };
+
+function findUser(props) {
+  const { _id, mail, idp, eppn, persistentId, uid, remoteUser } = props;
+
+  const query = { $or: [] };
+
+  if (_id)          { query.$or.push({ _id }); }
+  if (persistentId) { query.$or.push({ persistentId }); }
+
+  if (idp) {
+    const ids = [];
+
+    if (mail)       { ids.push({ mail }); }
+    if (eppn)       { ids.push({ eppn }); }
+    if (uid)        { ids.push({ uid }); }
+    if (remoteUser) { ids.push({ remoteUser }); }
+
+    if (ids.length > 0) {
+      query.$or.push({ idp, $or: ids });
+    }
+  }
+
+  return mongo.db.collection('users').findOne(query);
+}
