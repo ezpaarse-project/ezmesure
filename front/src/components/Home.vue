9063ed9f689dd520021a1e6a8cb515361b9532b0
Pseudom <dominique.lechaudel@gmail.com>
2016-11-16 12:07:01 +0100

    update URLs with new kibana path

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -10,14 +10,14 @@
 
     <p>Dashboard disponibles (sur préproduction) :</p>
     <ul>
-      <li><a href="https://ezmesure-preprod.couperin.org/goto/fcced8026a0038a11d9f4e1ebf1194ff">SR02 - Produit minimal viable</a></li>
-      <li><a href="https://ezmesure-preprod.couperin.org/goto/57a43ea15ce8a2778ebb7270a828790b">SR03 - Halte ! Kibana</a></li>
-      <li><a href="https://ezmesure-preprod.couperin.org/goto/619a800b97c54cb5738e749d94a3b4e2">SR03 - Détail par organisme</a></li>
-      <li><a href="https://ezmesure-preprod.couperin.org/goto/d728dc84dbff73e04ceb5031de25b7f1">SR03 - Agrégation par Unité</a></li>
-      <li><a href="https://ezmesure-preprod.couperin.org/goto/9c248838f1759d2767436977649d6c2e">SR05 - Université Franche-Comté - MVP début 2016</a></li>
-      <li><a href="https://ezmesure-preprod.couperin.org/goto/4ea85757ab136fc5d00282c680e7e09f">SR05 - Université de Lorraine - MVP début 2016</a></li>
-      <li><a href="https://ezmesure-preprod.couperin.org/goto/2d1d10bd03279a773ec3a2434277776c">SR05 - Comparaison interorganisme - MVP début 2016</a></li>
-      <li><a href="https://ezmesure-preprod.couperin.org/goto/d2211e5d0e891ad1e2f4b69f9973a92c">SR06 - Université de Rennes 1 - MVP début 2016</a></li>
+      <li><a href="https://ezmesure-preprod.couperin.org/kibana/goto/fcced8026a0038a11d9f4e1ebf1194ff">SR02 - Produit minimal viable</a></li>
+      <li><a href="https://ezmesure-preprod.couperin.org/kibana/goto/57a43ea15ce8a2778ebb7270a828790b">SR03 - Halte ! Kibana</a></li>
+      <li><a href="https://ezmesure-preprod.couperin.org/kibana/goto/619a800b97c54cb5738e749d94a3b4e2">SR03 - Détail par organisme</a></li>
+      <li><a href="https://ezmesure-preprod.couperin.org/kibana/goto/d728dc84dbff73e04ceb5031de25b7f1">SR03 - Agrégation par Unité</a></li>
+      <li><a href="https://ezmesure-preprod.couperin.org/kibana/goto/9c248838f1759d2767436977649d6c2e">SR05 - Université Franche-Comté - MVP début 2016</a></li>
+      <li><a href="https://ezmesure-preprod.couperin.org/kibana/goto/4ea85757ab136fc5d00282c680e7e09f">SR05 - Université de Lorraine - MVP début 2016</a></li>
+      <li><a href="https://ezmesure-preprod.couperin.org/kibana/goto/2d1d10bd03279a773ec3a2434277776c">SR05 - Comparaison interorganisme - MVP début 2016</a></li>
+      <li><a href="https://ezmesure-preprod.couperin.org/kibana/goto/d2211e5d0e891ad1e2f4b69f9973a92c">SR06 - Université de Rennes 1 - MVP début 2016</a></li>
     </ul>
     <p>Vous pouvez d'ores et déjà accéder à votre&nbsp;<router-link to="/profile">page de profil</router-link>.</p>
   </div>
