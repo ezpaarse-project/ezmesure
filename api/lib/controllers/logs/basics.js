b8627a072258f1f27d60707ba0e252a05481d358
Yannick Schurter <yannick.schurter@gmail.com>
2018-04-26 15:12:06 +0200

    top metrics initial version

--- api/lib/controllers/logs/basics.js
+++ api/lib/controllers/logs/basics.js
@@ -2,8 +2,8 @@ const elasticsearch = require('../../services/elastic');
 
 exports.list = async function (ctx) {
   ctx.action = 'indices/list';
-  ctx.type = 'json';
-  ctx.body = await elasticsearch.indices.stats({
+  ctx.type   = 'json';
+  ctx.body   = await elasticsearch.indices.stats({
     metric: 'docs',
     headers: { 'es-security-runas-user': ctx.state.user.username }
   });
@@ -11,7 +11,7 @@ exports.list = async function (ctx) {
 
 exports.del = async function (ctx, index) {
   ctx.action = 'indices/delete';
-  ctx.index = index;
+  ctx.index  = index;
   const username  = ctx.state.user.username;
   const perm      = await elasticsearch.hasPrivileges(username, [index], ['delete_index']);
   const canDelete = perm && perm.index && perm.index[index] && perm.index[index]['delete_index'];
@@ -26,3 +26,116 @@ exports.del = async function (ctx, index) {
     headers: { 'es-security-runas-user': username }
   });
 };
+
+/**
+ * Return aggregated metrics for a given index pattern
+ */
+exports.tops = async function (ctx, index) {
+  ctx.action = 'indices/tops';
+  ctx.type   = 'json';
+  ctx.index  = index;
+  const username = ctx.state.user.username;
+
+  const now          = new Date();
+  const currentYear  = now.getFullYear();
+  const currentMonth = now.getMonth();
+  const currentDate  = now.getDate();
+
+  const dateRange = {
+    min: new Date(0),
+    max: now
+  }
+
+  switch (ctx.query.period) {
+    case 'today':
+      dateRange.min = new Date(currentYear, currentMonth, currentDate)
+      break;
+    case 'yersteday':
+      dateRange.min = new Date(currentYear, currentMonth, currentDate - 2)
+      dateRange.max = new Date(currentYear, currentMonth, currentDate - 1)
+      break;
+    case 'current_week':
+      dateRange.min = new Date(currentYear, currentMonth, currentDate - 7)
+      break;
+    case 'last_week':
+      dateRange.min = new Date(currentYear, currentMonth, currentDate - 14)
+      dateRange.max = new Date(currentYear, currentMonth, currentDate - 7)
+      break;
+    case 'current_month':
+      dateRange.min = new Date(currentYear, currentMonth, 1)
+      break;
+    case 'last_month':
+      dateRange.min = new Date(currentYear, currentMonth - 1, 1)
+      dateRange.max = new Date(currentYear, currentMonth, 1)
+      break;
+    case 'current_year':
+      dateRange.min = new Date(currentYear, 0, 1)
+      break;
+    case 'last_year':
+      dateRange.min = new Date(currentYear - 1, 0, 1)
+      dateRange.max = new Date(currentYear, 0, 1)
+      break;
+    case 'all':
+      break;
+    default:
+      this.throw(400, 'Invalid period')
+      break;
+  }
+
+  const result = await elasticsearch.search({
+    index,
+    type: "event",
+    headers: { 'es-security-runas-user': username },
+    body: {
+      size: 0,
+      query: {
+        bool: {
+          must: [{
+            range: {
+              datetime: {
+                gte: dateRange.min.getTime(),
+                lte: dateRange.max.getTime(),
+                format: "epoch_millis"
+              }
+            }
+          }]
+        }
+      },
+      aggs : {
+        indices   : { terms: { field: "_index", size: 10 } },
+        titles    : { terms: { field: "publication_title", size: 10 } },
+        publishers: { terms: { field: "publisher_name", size: 10 } },
+        maxDate   : { max : { field: "datetime" } },
+        minDate   : { min : { field: "datetime" } }
+      }
+    }
+  })
+
+  const {
+    took,
+    hits = {},
+    aggregations = {}
+  } = result
+
+  const {
+    titles = {},
+    publishers = {},
+    indices = {},
+    minDate = {},
+    maxDate = {}
+  } = aggregations
+
+  ctx.body = {
+    took,
+    docs: hits.total,
+    dateCoverage: {
+      min: minDate.value,
+      max: maxDate.value
+    },
+    tops: {
+      titles: titles.buckets,
+      publishers: publishers.buckets,
+      indices: indices.buckets
+    }
+  }
+}
--- api/lib/controllers/logs/index.js
+++ api/lib/controllers/logs/index.js
@@ -1,13 +1,14 @@
 const Koa   = require('koa');
 const route = require('koa-route');
 
-const { list, del } = require('./basics');
+const { list, del, tops } = require('./basics');
 const upload = require('./upload');
 
 const app = new Koa();
 
 app.use(route.get('/', list));
-app.use(route.delete('/:orgName', del));
-app.use(route.post('/:orgName', upload));
+app.use(route.get('/:index/tops', tops));
+app.use(route.delete('/:index', del));
+app.use(route.post('/:index', upload));
 
 module.exports = app;
