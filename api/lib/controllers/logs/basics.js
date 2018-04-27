510551f6cfd397f5ce782d13c598f3cbae003017
Yannick Schurter <yannick.schurter@gmail.com>
2018-04-27 15:01:35 +0200

    add size option

--- api/lib/controllers/logs/basics.js
+++ api/lib/controllers/logs/basics.js
@@ -41,44 +41,52 @@ exports.tops = async function (ctx, index) {
   const currentMonth = now.getMonth();
   const currentDate  = now.getDate();
 
+  let size = parseInt(ctx.query.size);
+
+  if (isNaN(size)) {
+    size = 10;
+  } else {
+    size = Math.min(Math.abs(size), 50);
+  }
+
   const dateRange = {
     min: new Date(0),
     max: now
-  }
+  };
 
   switch (ctx.query.period) {
     case 'today':
-      dateRange.min = new Date(currentYear, currentMonth, currentDate)
+      dateRange.min = new Date(currentYear, currentMonth, currentDate);
       break;
     case 'yersteday':
-      dateRange.min = new Date(currentYear, currentMonth, currentDate - 2)
-      dateRange.max = new Date(currentYear, currentMonth, currentDate - 1)
+      dateRange.min = new Date(currentYear, currentMonth, currentDate - 2);
+      dateRange.max = new Date(currentYear, currentMonth, currentDate - 1);
       break;
     case 'current_week':
-      dateRange.min = new Date(currentYear, currentMonth, currentDate - 7)
+      dateRange.min = new Date(currentYear, currentMonth, currentDate - 7);
       break;
     case 'last_week':
-      dateRange.min = new Date(currentYear, currentMonth, currentDate - 14)
-      dateRange.max = new Date(currentYear, currentMonth, currentDate - 7)
+      dateRange.min = new Date(currentYear, currentMonth, currentDate - 14);
+      dateRange.max = new Date(currentYear, currentMonth, currentDate - 7);
       break;
     case 'current_month':
-      dateRange.min = new Date(currentYear, currentMonth, 1)
+      dateRange.min = new Date(currentYear, currentMonth, 1);
       break;
     case 'last_month':
-      dateRange.min = new Date(currentYear, currentMonth - 1, 1)
-      dateRange.max = new Date(currentYear, currentMonth, 1)
+      dateRange.min = new Date(currentYear, currentMonth - 1, 1);
+      dateRange.max = new Date(currentYear, currentMonth, 1);
       break;
     case 'current_year':
-      dateRange.min = new Date(currentYear, 0, 1)
+      dateRange.min = new Date(currentYear, 0, 1);
       break;
     case 'last_year':
-      dateRange.min = new Date(currentYear - 1, 0, 1)
-      dateRange.max = new Date(currentYear, 0, 1)
+      dateRange.min = new Date(currentYear - 1, 0, 1);
+      dateRange.max = new Date(currentYear, 0, 1);
       break;
     case 'all':
       break;
     default:
-      this.throw(400, 'Invalid period')
+      this.throw(400, 'Invalid period');
       break;
   }
 
@@ -102,20 +110,20 @@ exports.tops = async function (ctx, index) {
         }
       },
       aggs : {
-        indices   : { terms: { field: "_index", size: 10 } },
-        titles    : { terms: { field: "publication_title", size: 10 } },
-        publishers: { terms: { field: "publisher_name", size: 10 } },
+        indices   : { terms: { field: "_index", size } },
+        titles    : { terms: { field: "publication_title", size } },
+        publishers: { terms: { field: "publisher_name", size } },
         maxDate   : { max : { field: "datetime" } },
         minDate   : { min : { field: "datetime" } }
       }
     }
-  })
+  });
 
   const {
     took,
     hits = {},
     aggregations = {}
-  } = result
+  } = result;
 
   const {
     titles = {},
@@ -123,7 +131,7 @@ exports.tops = async function (ctx, index) {
     indices = {},
     minDate = {},
     maxDate = {}
-  } = aggregations
+  } = aggregations;
 
   ctx.body = {
     took,
@@ -137,5 +145,5 @@ exports.tops = async function (ctx, index) {
       publishers: publishers.buckets,
       indices: indices.buckets
     }
-  }
+  };
 }
