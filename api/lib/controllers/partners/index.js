ff8eec0c2f9e7669a3ea960d85ed6245e3ed7248
Yannick Schurter <yannick.schurter@gmail.com>
2018-11-15 17:35:16 +0100

    add auth to refresh route

--- api/lib/controllers/partners/index.js
+++ api/lib/controllers/partners/index.js
@@ -1,11 +1,16 @@
 const Koa = require('koa');
 const route = require('koa-route');
+const jwt = require('koa-jwt');
 
+const { auth } = require('config');
 const { list, refresh } = require('./actions');
 
 const app = new Koa();
 
 app.use(route.get('/', list));
+
+app.use(jwt({ secret: auth.secret, cookie: auth.cookie }));
+
 app.use(route.post('/refresh', refresh));
 
 module.exports = app;
