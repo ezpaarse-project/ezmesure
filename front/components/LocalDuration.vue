8579b7716939ef017d96af2559c02225cc3a1e58
Yannick Schurter <yannick.schurter@gmail.com>
2021-02-24 17:18:11 +0100

    feat: add a LocalDuration component

---
+++ front/components/LocalDuration.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>{{ duration }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    ms: {
+      type: Number,
+      default: () => 0,
+    },
+    includeSeconds: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+  computed: {
+    duration() {
+      return this.$dateFunctions.msToLocalDistance(this.ms, {
+        includeSeconds: this.includeSeconds,
+      });
+    },
+  },
+};
+</script>
