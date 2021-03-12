d537fa97369c079dcb9ed9941ca0016c44919fe3
Yannick Schurter <yannick.schurter@gmail.com>
2021-03-12 17:31:08 +0100

    refactor: require sushi_form role for sushi routes

--- api/lib/controllers/sushi/index.js
+++ api/lib/controllers/sushi/index.js
@@ -18,7 +18,12 @@ const {
   fetchSushi,
 } = require('./actions');
 
-router.use(requireJwt, requireUser, requireTermsOfUse, requireAnyRole(['sushi_form_tester', 'admin', 'superuser']));
+router.use(
+  requireJwt,
+  requireUser,
+  requireTermsOfUse,
+  requireAnyRole(['sushi_form', 'admin', 'superuser']),
+);
 
 router.route({
   method: 'GET',
