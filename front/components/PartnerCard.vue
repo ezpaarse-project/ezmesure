9f727b060eec804495bb114901379c82bdfd339c
Yannick Schurter <yannick.schurter@gmail.com>
2021-03-29 16:24:58 +0200

    feat: show partner links

--- front/components/PartnerCard.vue
+++ front/components/PartnerCard.vue
@@ -24,45 +24,77 @@
       </div>
     </v-card-text>
 
-    <v-divider v-if="hasAutomation" />
-
-    <v-card-text v-if="hasAutomation" class="text-center">
-      <div class="subtitle-1">
-        {{ $t('partners.automated') }}
-      </div>
-      <v-chip
-        v-for="auto in automations"
-        :key="auto.label"
-        :color="auto.color"
-        small
-        label
-        dark
-        class="mr-1 mb-1"
-      >
-        {{ $t(`partners.auto.${auto.label}`) }}
-      </v-chip>
-    </v-card-text>
-
     <v-divider />
 
     <v-card-text class="text-center">
       <div class="subtitle-1" v-text="$t('partners.correspondents')" />
 
-      <div v-for="contact in contacts" :key="contact.name">
-        <v-tooltip right>
-          <template v-slot:activator="{ on }">
-            <span v-on="on">{{ contact.name }}</span>
-          </template>
-          <span v-if="contact.type === 'tech'">
-            {{ $t('partners.technical') }}
-          </span>
-          <span v-else-if="contact.type === 'doc'">
-            {{ $t('partners.documentary') }}
-          </span>
-          <span v-else-if="contact.type === 'doc-tech'">
-            {{ $t('partners.technical') }} / {{ $t('partners.documentary') }}
-          </span>
-        </v-tooltip>
+      <template v-if="hasContacts">
+        <div v-for="contact in contacts" :key="contact.name">
+          <v-tooltip right>
+            <template v-slot:activator="{ on }">
+              <span v-on="on">{{ contact.name }}</span>
+            </template>
+            <span v-if="contact.type === 'tech'">
+              {{ $t('partners.technical') }}
+            </span>
+            <span v-else-if="contact.type === 'doc'">
+              {{ $t('partners.documentary') }}
+            </span>
+            <span v-else-if="contact.type === 'doc-tech'">
+              {{ $t('partners.technical') }} / {{ $t('partners.documentary') }}
+            </span>
+          </v-tooltip>
+        </div>
+      </template>
+
+      <div v-else class="font-italic">
+        {{ $t('partners.noContacts') }}
+      </div>
+    </v-card-text>
+
+    <v-divider />
+
+    <v-card-text class="text-center">
+      <div class="subtitle-1" v-text="$t('partners.automated')" />
+
+      <template v-if="hasAutomation">
+        <v-chip
+          v-for="auto in automations"
+          :key="auto.label"
+          :color="auto.color"
+          small
+          label
+          dark
+          class="mr-1 mb-1"
+        >
+          {{ $t(`partners.auto.${auto.label}`) }}
+        </v-chip>
+      </template>
+
+      <div v-else class="font-italic">
+        {{ $t('partners.noAutomations') }}
+      </div>
+    </v-card-text>
+
+    <v-divider />
+
+    <v-card-text class="text-center">
+      <div class="subtitle-1" v-text="$t('partners.links')" />
+
+      <template v-if="hasLinks">
+        <v-btn
+          v-for="link in links"
+          :key="link.icon"
+          :href="link.url"
+          icon
+        >
+          <v-icon>{{ link.icon }}</v-icon>
+        </v-btn>
+      </template>
+
+      <div v-else class="font-italic">
+        {{ $t('partners.noLinks') }}
       </div>
     </v-card-text>
   </v-card>
@@ -77,6 +109,9 @@ export default {
     },
   },
   computed: {
+    hasContacts() {
+      return this.contacts.length > 0;
+    },
     contacts() {
       const doc = this.partner?.docContactName;
       const tech = this.partner?.techContactName;
@@ -106,6 +141,30 @@ export default {
     },
     hasAutomation() {
       return this.automations.length > 0;
+    },
+    hasLinks() {
+      return this.links.length > 0;
+    },
+    links() {
+      const links = [];
+
+      if (this.partner?.website) {
+        links.push({ icon: 'mdi-open-in-new', url: this.partner?.website });
+      }
+      if (this.partner?.twitterUrl) {
+        links.push({ icon: 'mdi-twitter', url: this.partner?.twitterUrl });
+      }
+      if (this.partner?.linkedinUrl) {
+        links.push({ icon: 'mdi-linkedin', url: this.partner?.linkedinUrl });
+      }
+      if (this.partner?.youtubeUrl) {
+        links.push({ icon: 'mdi-youtube', url: this.partner?.youtubeUrl });
+      }
+      if (this.partner?.facebookUrl) {
+        links.push({ icon: 'mdi-facebook', url: this.partner?.facebookUrl });
+      }
+
+      return links;
     },
   },
 };
