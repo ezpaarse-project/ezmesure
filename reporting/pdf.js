84a6cb8745c58a3254f40ab8e9f5d92c5a9faf16
wilmouths <wilmouthsteven@gmail.com>
2021-05-28 15:17:11 +0200

    generate pdf & fix style

---
+++ reporting/pdf.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+const printPDF = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+  await page.goto('http://localhost:4000/reporting/render-bibCNRS');
+
+  const pdf = await page.pdf({
+    format: 'A4',
+  });
+
+  const ws = fs.createWriteStream(path.resolve(__dirname, 'monPDF.pdf'));
+  ws.write(pdf);
+  ws.close();
+
+  await browser.close();
+};
+
+printPDF();
