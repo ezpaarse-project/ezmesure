ffb25fdaef03fc7b93425862c29c233f26f3511a
Yannick Schurter <yannick.schurter@gmail.com>
2022-02-10 15:39:05 +0100

    feat: add a SushiEndpoint model

---
+++ api/lib/models/SushiEndpoint.js
@@ -0,0 +1,65 @@
+const { Joi } = require('koa-joi-router');
+const { typedModel, registerModel, getModel } = require('./TypedModel');
+
+const type = 'sushi-endpoint';
+
+const schema = {
+  id: Joi.string().trim().required(),
+  institutionId: Joi.string().trim(),
+  updatedAt: Joi.date(),
+  createdAt: Joi.date(),
+
+  vendor: Joi.string().trim().required(),
+  sushiUrl: Joi.string().trim().required(),
+  description: Joi.string().trim().allow(''),
+  companies: Joi.string().trim().allow(''),
+
+  requireCustomerId: Joi.boolean().default(false),
+  requireRequestorId: Joi.boolean().default(false),
+  requireApiKey: Joi.boolean().default(false),
+  isSushiCompliant: Joi.boolean().default(false),
+
+  params: Joi.array().items(Joi.object({
+    name: Joi.string().trim().required(),
+    value: Joi.string().trim().allow(''),
+  })),
+};
+
+const createSchema = {
+  ...schema,
+  id: Joi.any().strip(),
+  updatedAt: Joi.any().strip(),
+  createdAt: Joi.any().strip(),
+};
+
+const updateSchema = {
+  ...createSchema,
+  institutionId: Joi.any().strip(),
+};
+
+class SushiEndpoint extends typedModel(type, schema, createSchema, updateSchema) {
+  static async findByInstitutionId(institutionId, opts) {
+    const options = opts || {};
+
+    options.filters = Array.isArray(opts?.filters) ? opts.filters : [];
+    options.filters.push({ term: { [`${type}.institutionId`]: institutionId } });
+
+    return this.findAll(options);
+  }
+
+  static deleteByInstitutionId(institutionId) {
+    this.deleteByQuery({
+      filters: [
+        { term: { [`${type}.institutionId`]: institutionId } },
+      ],
+    });
+  }
+
+  getInstitution() {
+    if (!this.data?.institutionId) { return null; }
+    return getModel('institution').findById(this.data.institutionId);
+  }
+}
+
+registerModel(SushiEndpoint);
+module.exports = SushiEndpoint;
