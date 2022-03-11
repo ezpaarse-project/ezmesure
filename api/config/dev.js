1501d78eb4970a9f7978212be4c98167b57722ae
Yannick Schurter <yannick.schurter@gmail.com>
2022-03-11 15:15:49 +0100

    build: set console logger to verbose for dev env

--- api/config/dev.js
+++ api/config/dev.js
@@ -2,4 +2,11 @@ const defaultConfig = require('./default');
 
 module.exports = {
   ...defaultConfig,
+  logs: {
+    app: {
+      Console: {
+        level: 'verbose',
+      },
+    },
+  },
 };
