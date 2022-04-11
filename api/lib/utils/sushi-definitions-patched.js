469c9259a0bdc8b1dfea94848dbba2dd47708c8b
Yannick Schurter <yannick.schurter@gmail.com>
2022-04-11 13:20:28 +0200

    feat: make Publisher optionnal and nullable in counter reports

--- api/lib/utils/sushi-definitions-patched.js
+++ api/lib/utils/sushi-definitions-patched.js
@@ -3,26 +3,30 @@ const defSchema = require('./sushi-definitions.json');
 const {
   SUSHI_error_model: errorSchema,
   COUNTER_item_performance: itemPerfSchema,
+  COUNTER_database_usage: databaseUsage,
+  COUNTER_title_usage: titleUsage,
+  COUNTER_item_usage: itemUsage,
 } = defSchema.definitions;
+
+// Remove Publisher from required fields
+databaseUsage.required = databaseUsage.required.filter((field) => field !== 'Publisher');
+titleUsage.required = titleUsage.required.filter((field) => field !== 'Publisher');
+itemUsage.required = itemUsage.required.filter((field) => field !== 'Publisher');
+
+// Allow Publisher to be null
+databaseUsage.properties.Publisher = { anyOf: [databaseUsage.properties.Publisher, { type: 'null' }] };
+titleUsage.properties.Publisher = { anyOf: [titleUsage.properties.Publisher, { type: 'null' }] };
+itemUsage.properties.Publisher = { anyOf: [itemUsage.properties.Publisher, { type: 'null' }] };
 
 // Remove Severity from required fields
 errorSchema.required = errorSchema.required.filter((field) => field !== 'Severity');
-
 // Allow Severity to be an integer
-const severity = errorSchema.properties.Severity;
-
-errorSchema.properties.Severity = {
-  anyOf: [severity, { type: 'integer' }],
-};
+errorSchema.properties.Severity = { anyOf: [errorSchema.properties.Severity, { type: 'integer' }] };
 
 // Remove enum for metric type
 const instanceSchema = itemPerfSchema.properties.Instance;
 const metricType = instanceSchema.items.properties.Metric_Type;
-
-instanceSchema.items.properties.Metric_Type = {
-  ...metricType,
-  enum: undefined,
-};
+instanceSchema.items.properties.Metric_Type = { ...metricType, enum: undefined };
 
 // Remove enum for all first-level properties
 Object.values(defSchema.definitions).forEach((value) => {
