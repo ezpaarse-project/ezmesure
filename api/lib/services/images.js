c004b56bdce988b8e8c909b684ea35228f6a97d5
Yannick Schurter <yannick.schurter@gmail.com>
2023-03-02 18:02:13 +0100

    refactor: move the logo handler to a standalone service

---
+++ api/lib/services/images.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const fs = require('fs-extra');
+const sharp = require('sharp');
+const { randomBytes } = require('crypto');
+
+const logosDir = path.resolve(__dirname, '..', '..', 'uploads', 'logos');
+
+module.exports = class ImagesService {
+  static logoPath(logoId) {
+    return logoId && path.resolve(logosDir, logoId);
+  }
+
+  /**
+   * Remove a logo with a given ID
+   * @static
+   * @param {String} logoId
+   * @return {Promise}
+   */
+  static remove(logoId) {
+    if (logoId) {
+      return fs.remove(ImagesService.logoPath(logoId));
+    }
+  }
+
+  /**
+   * Take a Base64 image, resize it and store it
+   *
+   * @static
+   * @param {String} base64logo
+   */
+  static async storeLogo(base64logo) {
+    const logoId = `${randomBytes(16).toString('hex')}.png`;
+    const logoPath = path.resolve(logosDir, logoId);
+    const logoContent = Buffer.from(base64logo, 'base64');
+
+    await fs.ensureDir(logosDir);
+    await sharp(logoContent)
+      .resize({
+        width: 600,
+        height: 200,
+        fit: sharp.fit.inside,
+      })
+      .toFormat('png')
+      .toFile(logoPath);
+
+    return logoId;
+  }
+};
