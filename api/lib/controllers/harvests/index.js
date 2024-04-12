5050fe80fb9ef52573d4849e7a895f99cc954ef7
Yannick Schurter <yannick.schurter@gmail.com>
2024-04-12 17:29:34 +0200

    feat(api): add plenty of filters to the harvest route

--- api/lib/controllers/harvests/index.js
+++ api/lib/controllers/harvests/index.js
@@ -17,6 +17,15 @@ router.use(
   requireAdmin,
 );
 
+const {
+  includableFields,
+} = require('../../entities/harvest.dto');
+
+const stringOrArray = Joi.alternatives().try(
+  Joi.string().trim().min(1),
+  Joi.array().items(Joi.string().trim().min(1)).min(1),
+);
+
 router.route({
   method: 'GET',
   path: '/',
@@ -24,15 +33,26 @@ router.route({
     getHarvests,
   ],
   validate: {
-    query: {
+    query: Joi.object({
       from: Joi.string().regex(/^[0-9]{4}-[0-9]{2}$/),
       to: Joi.string().regex(/^[0-9]{4}-[0-9]{2}$/),
-      reportId: Joi.string().trim(),
+      harvestedBefore: Joi.date().iso(),
+      harvestedAfter: Joi.date().iso(),
+      reportId: stringOrArray,
+      credentialsId: stringOrArray,
+      endpointId: stringOrArray,
+      status: stringOrArray,
+      errorCode: stringOrArray,
+      institutionId: stringOrArray,
+      tags: stringOrArray,
+      packages: stringOrArray,
       size: Joi.number().min(0),
       page: Joi.number().min(1),
       sort: Joi.string(),
       order: Joi.string().valid('asc', 'desc'),
-    },
+      distinct: stringOrArray,
+      include: Joi.array().single().items(Joi.string().valid(...includableFields)),
+    }).rename('include[]', 'include'),
   },
 });
 
