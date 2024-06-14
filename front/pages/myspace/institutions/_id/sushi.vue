0d7e040f809de315604731c522bb3efcb8f2b484
oxypomme <oxytom8@gmail.com>
2024-06-14 09:17:42 +0200

    fix(front): using new params for sushi of instituions

--- front/pages/myspace/institutions/_id/sushi.vue
+++ front/pages/myspace/institutions/_id/sushi.vue
@@ -976,7 +976,7 @@ export default {
       this.refreshing = true;
 
       try {
-        this.sushiItems = await this.$axios.$get(`/institutions/${this.institution.id}/sushi`, { params: { include: ['harvests'] } });
+        this.sushiItems = await this.$axios.$get(`/institutions/${this.institution.id}/sushi`, { params: { include: ['endpoint', 'harvests'], size: 0 } });
       } catch (e) {
         this.$store.dispatch('snacks/error', this.$t('institutions.sushi.unableToRetriveSushiData'));
       }
