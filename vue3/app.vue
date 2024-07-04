ccf67fea9f4e7ac34e1f97b7b9eac07dd2cc2027
oxypomme <oxytom8@gmail.com>
2024-07-04 10:31:47 +0200

    feat(vue3): added default head

--- vue3/app.vue
+++ vue3/app.vue
@@ -7,3 +7,17 @@
     </ClientOnly>
   </v-app>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'ezMESURE - Plateforme des tableaux de bord ezPAARSE de l’ESR',
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: 'ezMESURE est un entrepôt national centralisant les statistiques d’usage de la documentation scientifique numérique des établissements de l’enseignement supérieur et de la recherche (ESR). ezMESURE aggrege les données produites par les installations ezPAARSE des établissements de l’ESR.' },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  ],
+});
+</script>
