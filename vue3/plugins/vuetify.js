d2f40143105fe6220dd4ef5cc32d0f9d3b0aa7b5
oxypomme <oxytom8@gmail.com>
2024-07-08 16:10:26 +0200

    feat(vue3): vuetify now use global i18n

--- vue3/plugins/vuetify.js
+++ vue3/plugins/vuetify.js
@@ -1,25 +1,45 @@
+import { defineNuxtPlugin, useI18n } from '#imports';
 import { createVuetify } from 'vuetify';
+
+/* eslint-disable import/no-unresolved */
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n';
+import { fr, en } from 'vuetify/locale';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import colors from 'vuetify/util/colors';
+/* eslint-enable import/no-unresolved */
+
+const I18N_KEY = '__VUE_I18N__';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify({
-    ssr: true,
-    components,
-    directives,
-    theme: {
-      themes: {
-        light: {
-          colors: {
-            primary: colors.purple.base,
-            secondary: colors.grey.darken2,
-            accent: colors.lightBlue.base,
+  // Merge Vuetify messages with i18n
+  const i18n = nuxtApp.vueApp[I18N_KEY];
+  i18n.global.mergeLocaleMessage('en', { $vuetify: en });
+  i18n.global.mergeLocaleMessage('fr', { $vuetify: fr });
+
+  // Setup Vuetify
+  nuxtApp.vueApp.use(
+    createVuetify({
+      ssr: true,
+      components,
+      directives,
+      locale: {
+        adapter: createVueI18nAdapter({
+          i18n,
+          useI18n,
+        }),
+      },
+      theme: {
+        themes: {
+          light: {
+            colors: {
+              primary: colors.purple.base,
+              secondary: colors.grey.darken2,
+              accent: colors.lightBlue.base,
+            },
           },
         },
       },
-    },
-  });
-
-  nuxtApp.vueApp.use(vuetify);
+    }),
+  );
 });
