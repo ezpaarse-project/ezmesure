49246a6095fafb12581dc73b1b408dcc819cf415
oxypomme <oxytom8@gmail.com>
2024-08-06 09:37:15 +0200

    feat(vue3): added error page

---
+++ vue3/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <ClientOnly>
+      <SkeletonAppBar @model-value:menu="updateVisibleMenu()" />
+
+      <v-main>
+        <SkeletonPageLoader
+          :error="error"
+          show
+          show-refresh
+          @click:refresh="handleError()"
+        />
+      </v-main>
+    </ClientOnly>
+  </v-app>
+</template>
+
+<script setup>
+defineProps({
+  error: {
+    type: Object,
+    required: true,
+  },
+});
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
