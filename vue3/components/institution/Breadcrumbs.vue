7da76dfa9f572dfcc8c8fa1e2738f6d22ed4ff74
oxypomme <oxytom8@gmail.com>
2024-08-13 09:21:32 +0200

    fix(vue3): fixed width of InstitutionBreadcrumbs

--- vue3/components/institution/Breadcrumbs.vue
+++ vue3/components/institution/Breadcrumbs.vue
@@ -1,9 +1,15 @@
 <template>
-  <nuxt-link :to="institutionLink">
-    {{ institution.name }}
-  </nuxt-link>
-  /
-  {{ current }}
+  <div class="d-flex pr-4 pr-lg-8">
+    <div class="text-truncate">
+      <nuxt-link :to="institutionLink" style="max-width: 10px;">
+        {{ institution.name }}
+      </nuxt-link>
+    </div>
+    <div class="mx-1">
+      /
+    </div>
+    <div>{{ current }}</div>
+  </div>
 </template>
 
 <script setup>
