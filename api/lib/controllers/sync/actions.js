bc6d4db7344215101c6a7e8cf11526b171d28565
oxypomme <oxytom8@gmail.com>
2024-09-23 15:25:25 +0200

    feat(api): added expected data when getting sync status

--- api/lib/controllers/sync/actions.js
+++ api/lib/controllers/sync/actions.js
@@ -1,3 +1,8 @@
+const UsersService = require('../../entities/users.service');
+const InstitutionsService = require('../../entities/institutions.service');
+const SpacesService = require('../../entities/spaces.service');
+const RepositoriesService = require('../../entities/repositories.service');
+
 const {
   startSync,
   isSynchronizing,
@@ -21,5 +26,21 @@ exports.startSync = async (ctx) => {
 exports.getSyncStatus = async (ctx) => {
   ctx.type = 'json';
   ctx.status = 200;
-  ctx.body = getStatus();
+
+  const expectedServices = {
+    users: { service: new UsersService() },
+    institutions: { service: new InstitutionsService(), where: { validated: true } },
+    spaces: { service: new SpacesService() },
+    repositories: { service: new RepositoriesService() },
+  };
+
+  const entries = await Promise.all(
+    Object.entries(expectedServices)
+      .map(([k, { service, where }]) => service.count({ where }).then((v) => [k, v])),
+  );
+
+  ctx.body = {
+    data: getStatus(),
+    expected: Object.fromEntries(entries),
+  };
 };
