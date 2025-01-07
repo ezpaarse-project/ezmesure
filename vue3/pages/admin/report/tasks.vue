3978f79172602e7bf48117875870fe22875a37d0
oxypomme <oxytom8@gmail.com>
2025-01-07 09:25:49 +0100

    feat(vue3): added ezreeport pages

---
+++ vue3/pages/admin/report/tasks.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <SkeletonPageLoader
+      v-if="error"
+      :error="error"
+      show
+      show-refresh
+      @click:refresh="refresh()"
+    />
+
+    <div v-else>
+      <ezr-task-table :title-prefix="`${$t('menu.adminReport')} / `">
+        <template #prepend>
+          <v-btn
+            icon="mdi-menu"
+            @click="toggle()"
+          />
+        </template>
+
+        <template #[`item.namespace`]="{ namespace }">
+          <nuxt-link :to="`/admin/institutions/${namespace.id}/reports`">
+            {{ namespace.name }}
+          </nuxt-link>
+        </template>
+      </ezr-task-table>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth', 'terms', 'admin'],
+});
+
+const { toggle } = useDrawerStore();
+
+const { error } = await useEzr();
+</script>
---
+++ vue3/pages/admin/report/templates.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <SkeletonPageLoader
+      v-if="error"
+      :error="error"
+      show
+      show-refresh
+      @click:refresh="refresh()"
+    />
+
+    <div v-else>
+      <ezr-template-table :title-prefix="`${$t('menu.adminReport')} / `">
+        <template #prepend>
+          <v-btn
+            icon="mdi-menu"
+            @click="toggle()"
+          />
+        </template>
+      </ezr-template-table>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth', 'terms', 'admin'],
+});
+
+const { toggle } = useDrawerStore();
+
+const { error } = await useEzr();
+</script>
