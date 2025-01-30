fa3e650d7b12869ab85c193e8c769471b7abd196
oxypomme <oxytom8@gmail.com>
2025-01-30 15:42:41 +0100

    feat(api): added sync betweens models and elastic

---
+++ api/lib/services/prisma/repository-alias-permissions.js
@@ -0,0 +1,108 @@
+// @ts-check
+const { client: prisma, Prisma } = require('./index');
+
+/* eslint-disable max-len */
+/**
+ * @typedef {import('@prisma/client').Prisma.TransactionClient} TransactionClient
+ * @typedef {import('@prisma/client').RepositoryAliasPermission} RepositoryAliasPermission
+ * @typedef {import('@prisma/client').Prisma.RepositoryAliasPermissionUpdateArgs} RepositoryAliasPermissionUpdateArgs
+ * @typedef {import('@prisma/client').Prisma.RepositoryAliasPermissionUpsertArgs} RepositoryAliasPermissionUpsertArgs
+ * @typedef {import('@prisma/client').Prisma.RepositoryAliasPermissionFindUniqueArgs} RepositoryAliasPermissionFindUniqueArgs
+ * @typedef {import('@prisma/client').Prisma.RepositoryAliasPermissionFindManyArgs} RepositoryAliasPermissionFindManyArgs
+ * @typedef {import('@prisma/client').Prisma.RepositoryAliasPermissionCreateArgs} RepositoryAliasPermissionCreateArgs
+ * @typedef {import('@prisma/client').Prisma.RepositoryAliasPermissionDeleteArgs} RepositoryAliasPermissionDeleteArgs
+ */
+/* eslint-enable max-len */
+
+/**
+ * @param {RepositoryAliasPermissionCreateArgs} params
+ * @param {TransactionClient} [tx]
+ * @returns {Promise<RepositoryAliasPermission>}
+ */
+function create(params, tx = prisma) {
+  return tx.repositoryAliasPermission.create(params);
+}
+
+/**
+ * @param {RepositoryAliasPermissionFindManyArgs} params
+ * @param {TransactionClient} [tx]
+ * @returns {Promise<RepositoryAliasPermission[]>}
+ */
+function findMany(params, tx = prisma) {
+  return tx.repositoryAliasPermission.findMany(params);
+}
+
+/**
+ * @param {RepositoryAliasPermissionFindUniqueArgs} params
+ * @param {TransactionClient} [tx]
+ * @returns {Promise<RepositoryAliasPermission | null>}
+ */
+function findUnique(params, tx = prisma) {
+  return tx.repositoryAliasPermission.findUnique(params);
+}
+
+/**
+ * @param {string} institutionId
+ * @param {string} pattern
+ * @param {string} username
+ * @param {TransactionClient} [tx]
+ * @returns {Promise<RepositoryAliasPermission | null>}
+ */
+function findById(institutionId, pattern, username, tx = prisma) {
+  return tx.repositoryAliasPermission.findUnique({
+    where: {
+      username_institutionId_aliasPattern: {
+        username,
+        institutionId,
+        aliasPattern: pattern,
+      },
+    },
+  });
+}
+
+/**
+ * @param {RepositoryAliasPermissionUpdateArgs} params
+ * @param {TransactionClient} [tx]
+ * @returns {Promise<RepositoryAliasPermission>}
+ */
+function update(params, tx = prisma) {
+  return tx.repositoryAliasPermission.update(params);
+}
+
+/**
+ * @param {RepositoryAliasPermissionUpsertArgs} params
+ * @param {TransactionClient} [tx]
+ * @returns {Promise<RepositoryAliasPermission>}
+ */
+function upsert(params, tx = prisma) {
+  return tx.repositoryAliasPermission.upsert(params);
+}
+
+/**
+ * @param {RepositoryAliasPermissionDeleteArgs} params
+ * @param {TransactionClient} [tx]
+ * @returns {Promise<RepositoryAliasPermission | null>}
+ */
+async function remove(params, tx = prisma) {
+  let permission;
+  try {
+    permission = await tx.repositoryAliasPermission.delete(params);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+      return null;
+    }
+    throw error;
+  }
+
+  return permission;
+}
+
+module.exports = {
+  create,
+  findMany,
+  findUnique,
+  findById,
+  update,
+  upsert,
+  remove,
+};
--- api/lib/services/sync/elastic.js
+++ api/lib/services/sync/elastic.js
@@ -4,6 +4,7 @@ const { CronJob } = require('cron');
 const { appLogger } = require('../logger');
 
 const RepositoriesService = require('../../entities/repositories.service');
+const RepositoryAliasesService = require('../../entities/repository-aliases.service');
 const SpacesService = require('../../entities/spaces.service');
 const UsersService = require('../../entities/users.service');
 
@@ -17,6 +18,8 @@ const { execThrottledPromises } = require('../promises');
 
 const { upsertRole, deleteRole } = require('../elastic/roles');
 const { upsertUser } = require('../elastic/users');
+const { upsertAlias } = require('../elastic/indices');
+const { filtersToESQuery } = require('../elastic/filters');
 
 const { syncSchedule } = config.get('elasticsearch');
 
@@ -25,6 +28,7 @@ const { syncSchedule } = config.get('elasticsearch');
  * @typedef {import('@prisma/client').User} User
  * @typedef {import('@elastic/elasticsearch').estypes.SecurityUser} ElasticUser
  * @typedef {import('@prisma/client').Repository} Repository
+ * @typedef {import('@prisma/client').RepositoryAlias} RepositoryAlias
  */
 
 /**
@@ -93,6 +97,81 @@ const syncRepository = async (repo) => {
 };
 
 /**
+ * Remove roles associated to a repository alias
+ * @param {RepositoryAlias} alias - The repository to sync
+ * @returns {Promise<void>}
+ */
+const unmountAlias = async (alias) => {
+  const repositoryService = new RepositoriesService();
+  const repo = await repositoryService.findUnique({ where: { pattern: alias.target } });
+  if (!repo) {
+    appLogger.error(`[elastic] Cannot create alias [${alias.pattern}], repository [${alias.target}] not found`);
+    return;
+  }
+
+  const readOnlyRole = generateRoleNameFromRepository(repo, 'readonly');
+
+  try {
+    await deleteRole(readOnlyRole);
+    appLogger.verbose(`[elastic] Role [${readOnlyRole}] has been deleted`);
+  } catch (error) {
+    appLogger.error(`[elastic] Role [${readOnlyRole}] cannot be deleted:\n${error}`);
+  }
+};
+
+/**
+ * Synchronize a repository alias with Elasticsearch, making sure that associated roles exists
+ * @param {RepositoryAlias} alias - The repository to sync
+ * @returns {Promise<void>}
+ */
+const syncAlias = async (alias) => {
+  const repositoryService = new RepositoriesService();
+  const repo = await repositoryService.findUnique({ where: { pattern: alias.target } });
+  if (!repo) {
+    appLogger.error(`[elastic] Cannot create alias [${alias.pattern}], repository [${alias.target}] not found`);
+    return;
+  }
+
+  const readOnlyRole = generateRoleNameFromRepository({ ...repo, pattern: alias.pattern }, 'readonly');
+
+  try {
+    await upsertRole(readOnlyRole, [alias.pattern], ['read', 'view_index_metadata']);
+    appLogger.verbose(`[elastic] Role [${readOnlyRole}] has been upserted`);
+  } catch (error) {
+    appLogger.error(`[elastic] Role [${readOnlyRole}] cannot be upserted:\n${error}`);
+  }
+
+  let filters;
+  if (alias.filters) {
+    filters = filtersToESQuery(alias.filters);
+  }
+
+  try {
+    await upsertAlias(alias.pattern, repo.pattern, filters);
+    appLogger.verbose(`[elastic] Alias [${alias.pattern}] has been upserted`);
+  } catch (error) {
+    appLogger.error(`[elastic] Alias [${alias.pattern}] cannot be upserted:\n${error}`);
+  }
+
+  const spacesService = new SpacesService();
+
+  const spacesOfSameType = await spacesService.findMany({
+    where: {
+      type: repo.type,
+      institution: {
+        repositoryAliases: {
+          some: {
+            pattern: alias.pattern,
+          },
+        },
+      },
+    },
+  });
+
+  await Promise.allSettled(spacesOfSameType.map((space) => syncIndexPatterns(space)));
+};
+
+/**
  * Sync Elastic's roles to ezMESURE's repositories
  * @returns {Promise<ThrottledPromisesResult>}
  */
@@ -107,6 +186,25 @@ const syncRepositories = async () => {
     (error) => appLogger.warn(`[elastic] Error on upserting repositories roles: ${error.message}`),
   );
   appLogger.verbose(`[elastic] Upserted ${res.fulfilled} repositories roles (${res.errors} errors)`);
+
+  return res;
+};
+
+/**
+ * Sync Elastic's roles to ezMESURE's repository aliases
+ * @returns {Promise<ThrottledPromisesResult>}
+ */
+const syncRepositoryAliases = async () => {
+  const repositoryAliasesService = new RepositoryAliasesService();
+  const aliases = await repositoryAliasesService.findMany({});
+
+  const executors = aliases.map((alias) => () => syncAlias(alias));
+
+  const res = await execThrottledPromises(
+    executors,
+    (error) => appLogger.warn(`[elastic] Error on upserting repository aliases roles: ${error.message}`),
+  );
+  appLogger.verbose(`[elastic] Upserted ${res.fulfilled} repository aliases roles (${res.errors} errors)`);
 
   return res;
 };
@@ -177,8 +275,11 @@ module.exports = {
   startCron,
   sync,
   syncRepository,
+  syncAlias,
   syncRepositories,
+  syncRepositoryAliases,
   syncUser,
   syncUsers,
   unmountRepository,
+  unmountAlias,
 };
