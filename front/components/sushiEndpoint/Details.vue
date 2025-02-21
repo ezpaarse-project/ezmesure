1fa141fff379be580f858286d5605c9e5d6e208f
oxypomme <oxytom8@gmail.com>
2025-02-21 15:29:28 +0100

    feat(front): added link to COUNTER registry in endpoint details

--- front/components/sushiEndpoint/Details.vue
+++ front/components/sushiEndpoint/Details.vue
@@ -61,8 +61,15 @@ const fields = computed(() => [
   { value: props.modelValue.sushiUrl || '', label: t('endpoints.url'), cols: 8 },
   { value: props.modelValue.technicalProvider || '-', label: t('endpoints.technicalProvider'), cols: 4 },
 
-  { value: props.modelValue.requireCustomerId ? t('yes') : t('no'), label: t('institutions.sushi.requestorId'), cols: 4 },
-  { value: props.modelValue.requireRequestorId ? t('yes') : t('no'), label: t('institutions.sushi.customerId'), cols: 4 },
-  { value: props.modelValue.requireApiKey ? t('yes') : t('no'), label: t('institutions.sushi.apiKey'), cols: 4 },
-]);
+  { value: props.modelValue.requireCustomerId ? t('yes') : t('no'), label: t('endpoints.requireRequestorId'), cols: 4 },
+  { value: props.modelValue.requireRequestorId ? t('yes') : t('no'), label: t('endpoints.requireCustomerId'), cols: 4 },
+  { value: props.modelValue.requireApiKey ? t('yes') : t('no'), label: t('endpoints.requireApiKey'), cols: 4 },
+
+  {
+    value: `https://registry.countermetrics.org/platform/${props.modelValue.registryId}`,
+    label: t('endpoints.registryUrl'),
+    cols: 12,
+    hide: !props.modelValue.registryId,
+  },
+].filter((v) => v.hide !== true));
 </script>
