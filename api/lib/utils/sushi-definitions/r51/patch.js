2a240eee7703c55b9181a97424bae02b62f9348c
oxypomme <oxytom8@gmail.com>
2025-07-22 11:11:20 +0200

    fix(api): allow dates without day and any string in registry urls in R5.1 validation

--- api/lib/utils/sushi-definitions/r51/patch.js
+++ api/lib/utils/sushi-definitions/r51/patch.js
@@ -3,21 +3,43 @@ const defSchema = require('./schema.json');
 // Patches definitions
 
 /**
- * Fix Registry_Record regex to allow old registry url
+ * Fix Registry_Record regex to allow any string, as we don't use it in ezMESURE
  *
  * Update by reference
  *
  * @param  {...object} schemas to fix
  */
 function fixRegistryRecord(...schemas) {
-  const allowedHosts = ['registry.countermetrics.org', 'registry.projectcounter.org'];
-  const allowedRegex = `(${allowedHosts.join('|')})`;
-
   // eslint-disable-next-line no-restricted-syntax
   for (const schema of schemas) {
     if (schema?.properties?.Registry_Record) {
       const { Registry_Record: item } = schema.properties;
-      item.pattern = item.pattern.replace(/registry.countermetrics.org/ig, allowedRegex);
+      item.pattern = undefined;
+    }
+  }
+}
+
+/**
+ * Fix Begin_Date and End_Date to allow not providing day of date
+ *
+ * Update by reference
+ *
+ * @param  {...object} schemas to fix
+ */
+function fixPeriodDateFormat(...schemas) {
+  const pattern = '^([0-9]{4})-([0-9]{2})(-[0-9]{2})?$';
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const schema of schemas) {
+    if (schema?.properties?.Begin_Date) {
+      const { Begin_Date: item } = schema.properties;
+      item.format = undefined;
+      item.pattern = pattern;
+    }
+    if (schema?.properties?.End_Date) {
+      const { End_Date: item } = schema.properties;
+      item.format = undefined;
+      item.pattern = pattern;
     }
   }
 }
@@ -27,6 +49,10 @@ function fixRegistryRecord(...schemas) {
 fixRegistryRecord(
   defSchema.definitions.Status,
   defSchema.definitions.Base_Report_Header,
+);
+
+fixPeriodDateFormat(
+  defSchema.definitions.Base_Report_Filters,
 );
 
 module.exports = defSchema;
