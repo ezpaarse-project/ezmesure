f870840e5b0d45d2f6e04750f2848f27842cc941
oxypomme <oxytom8@gmail.com>
2025-08-22 13:37:08 +0200

    feat(front): added fallback and message on auth page

--- front/app/pages/authenticate.vue
+++ front/app/pages/authenticate.vue
@@ -2,8 +2,8 @@
   <v-container class="fill-height">
     <v-row class="justify-center">
       <v-empty-state
-        :title="$t('authenticate.loading.title')"
-        :text="$t('authenticate.loading.text')"
+        :title="$t('pleaseWait')"
+        :text="$t('authenticate.loadingDesc')"
       >
         <template #media>
           <v-progress-circular
@@ -14,6 +14,26 @@
           >
             <v-icon icon="mdi-login" />
           </v-progress-circular>
+        </template>
+
+        <template #actions>
+          <v-slide-y-transition>
+            <v-row v-if="userIsWaiting" class="text-center mx-auto" style="max-width: 500px;">
+              <v-col cols="12" class="text-body-2">
+                {{ $t('authenticate.longLoadingDesc') }}
+              </v-col>
+
+              <v-col>
+                <v-btn
+                  :text="$t('authenticate.longLoadingBtn')"
+                  :href="redirectUrl.href"
+                  append-icon="mdi-login-variant"
+                  variant="outlined"
+                  color="primary"
+                />
+              </v-col>
+            </v-row>
+          </v-slide-y-transition>
         </template>
       </v-empty-state>
     </v-row>
@@ -34,10 +54,33 @@ if (status.value === 'authenticated') {
   await navigateTo(query?.redirect || '/myspace');
 }
 
-onMounted(async () => {
+let timeoutId;
+const userIsWaiting = shallowRef(false);
+
+const redirectUrl = computed(() => {
+  const url = new URL('/api/auth/oauth/login', window.location);
+  if (query?.redirect) { url.searchParams.set('origin', query.redirect); }
+
+  return url;
+});
+
+async function goToLogin() {
+  userIsWaiting.value = false;
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+
   await navigateTo(
-    { path: '/api/auth/oauth/login', query: { origin: query?.redirect } },
+    { path: redirectUrl.value.pathname, query: Object.fromEntries(redirectUrl.value.searchParams) },
     { external: true },
   );
+
+  timeoutId = setTimeout(() => {
+    userIsWaiting.value = true;
+  }, 5000);
+}
+
+onMounted(async () => {
+  await goToLogin();
 });
 </script>
