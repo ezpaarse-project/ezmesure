5ca5b6fd1239f2933075a1bb78f66de8fdc152fe
oxypomme <oxytom8@gmail.com>
2025-10-23 10:34:36 +0200

    refactor(api): splitted prisma schema in multiple files

---
+++ api/prisma.config.js
@@ -0,0 +1,8 @@
+const { join } = require('node:path');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { defineConfig } = require('prisma/config');
+
+export default defineConfig({
+  schema: join('prisma'),
+});
