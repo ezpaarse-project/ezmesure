f286ddf5033c39619942b671030a2ff63e8a2aec
yannick.schurter <yannick.schurter@gmail.com>
2025-11-21 17:29:18 +0100

    refactor(front): remove roleColors

--- front/app/utils/stateColors.js
+++ front/app/utils/stateColors.js
@@ -9,12 +9,6 @@ export const serviceColors = new Map([
   ['ezreeport', 'blue'],
 ]);
 
-export const roleColors = new Map([
-  ['contact:tech', { color: 'blue', icon: 'mdi-wrench' }],
-  ['contact:doc', { color: 'green', icon: 'mdi-book-open-variant' }],
-  ['guest', { icon: 'mdi-badge-account' }],
-]);
-
 export const sushiStatus = new Map([
   ['success', { color: 'green', icon: 'mdi-check' }],
   ['untested', { color: 'grey', icon: 'mdi-lan-pending' }],
