2303f37e70a95fb5900d9fd1e27d99b7d542070c
oxypomme <oxytom8@gmail.com>
2025-12-03 08:20:44 +0100

    refactor(api): using mjs extension for tests
    That help node identify the type of file, and therefore avoiding the
    need to disable the MODULE_TYPELESS_PACKAGE_JSON warning

--- api/vitest.config.js
+++ api/vitest.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     fileParallelism: false,
     env: {
-      NODE_OPTIONS: '--disable-warning=DEP0128 --disable-warning=MODULE_TYPELESS_PACKAGE_JSON'
-    }
+      NODE_OPTIONS: '--disable-warning=DEP0128',
+    },
   },
-})
+});
