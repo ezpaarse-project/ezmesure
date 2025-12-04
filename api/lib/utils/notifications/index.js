a0857440f2b29758a433fd5493d80766a5a5d425
oxypomme <oxytom8@gmail.com>
2025-12-04 17:12:00 +0100

    fix(api): fixed issues with NOT and arrays

--- api/lib/utils/notifications/index.js
+++ api/lib/utils/notifications/index.js
@@ -1,22 +1,34 @@
 // @ts-check
-const UserService = require('../../entities/users.service');
+const { client: prisma } = require('../../services/prisma');
+
+/** @typedef {import('../../.prisma/client.mjs').Prisma.TransactionClient} TransactionClient */
+
+/**
+ * Get prisma query to fetch users that are concerned by a type of notification
+ *
+ * @param {string} type - The type of notification
+ *
+ * @returns The query to get users from prisma
+ */
+const getNotificationUserWhere = (type) => ({
+  NOT: {
+    excludeNotifications: { has: type },
+  },
+});
 
 /**
  * Get list of admin emails concerned by type of notification (or all if not provided)
  *
- * @param {string} [type] - The type of notification
+ * @param {string} [notificationType] - The type of notification
+ * @param {TransactionClient} [tx]
  *
  * @returns {Promise<string[]>} The emails of admins
  */
-module.exports.getNotificationRecipients = async (type) => {
-  const users = new UserService();
-
-  const targets = await users.findMany({
+module.exports.getNotificationRecipients = async (notificationType, tx = prisma) => {
+  const targets = await tx.user.findMany({
     where: {
       isAdmin: true,
-      NOT: {
-        excludeNotifications: { has: type },
-      },
+      ...(notificationType ? getNotificationUserWhere(notificationType) : {}),
     },
     select: { email: true },
   });
@@ -27,21 +39,17 @@ module.exports.getNotificationRecipients = async (type) => {
 /**
  * Get prisma query to fetch memberships that are concerned by a type of notification
  *
- * @param {string} [type] - The type of notification
+ * @param {string} notificationType - The type of notification
  *
  * @returns The query to get memberships from prisma
  */
-module.exports.getNotificationMembershipWhere = (type) => ({
+module.exports.getNotificationMembershipWhere = (notificationType) => ({
   roles: {
     some: {
       role: {
-        notifications: { has: type },
+        notifications: { has: notificationType },
       },
     },
   },
-  user: {
-    NOT: {
-      excludeNotifications: { has: type },
-    },
-  },
+  user: getNotificationUserWhere(notificationType),
 });
