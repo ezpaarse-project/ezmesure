c0bad2167cbc82469a8ed1cb9841ee4494803519
oxypomme <oxytom8@gmail.com>
2026-01-09 18:32:37 +0100

    fix(api): fixed notification cron needing old env var

--- api/lib/services/crons/notifications.js
+++ api/lib/services/crons/notifications.js
@@ -5,20 +5,22 @@ const config = require('config');
 const { appLogger } = require('../logger');
 const { sendNotifications } = require('../notifications');
 
-const { recipients, cron } = config.get('notifications');
+const { cron } = config.get('notifications');
 
 const startBroadcastCron = () => {
-  const job = new CronJob(cron, () => {
-    sendNotifications().catch((err) => {
-      appLogger.error(`Failed to broadcast recent activity : ${err}`);
-    });
+  const job = CronJob.from({
+    cronTime: cron,
+    runOnInit: false,
+    onTick: async () => {
+      try {
+        sendNotifications();
+      } catch (err) {
+        appLogger.error(`Failed to broadcast recent activity : ${err}`);
+      }
+    },
   });
 
-  if (recipients) {
-    job.start();
-  } else {
-    appLogger.warn('No recipient configured, notifications will be disabled');
-  }
+  job.start();
 };
 
 module.exports = {
