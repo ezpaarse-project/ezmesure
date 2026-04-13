83d69a1ea627d1948bb5847437b01a76d36cc587
yannick.schurter <yannick.schurter@gmail.com>
2026-04-13 18:10:59 +0200

    feat(front): update profile when the user switches language

--- front/app/components/skeleton/lang/List.vue
+++ front/app/components/skeleton/lang/List.vue
@@ -4,7 +4,7 @@
       v-for="lang in locales"
       :key="lang.code"
       :title="lang.name"
-      @click="setLocale(lang.code)"
+      @click="changeLocale(lang.code)"
     >
       <template #prepend>
         <img
@@ -21,5 +21,22 @@
 <script setup>
 import locales from '@/lib/locales';
 
-const { setLocale } = useI18n();
+const { t, setLocale } = useI18n();
+const { status } = useAuth();
+const snacks = useSnacksStore();
+
+const changeLocale = async (lang) => {
+  setLocale(lang);
+
+  if (status === 'authenticated') {
+    try {
+      await $fetch('/api/profile/language', {
+        method: 'PUT',
+        body: { value: lang },
+      });
+    } catch (err) {
+      snacks.error(t('anErrorOccurred'), err);
+    }
+  }
+};
 </script>
